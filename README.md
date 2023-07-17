69354ccee219301a85252821ac02badfb726d47c
Thinus Swart <thinus.swart@gmail.com>
2023-07-17 10:00:47 +0200

    Initial upload of entire project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# dropboxsign_example
+# Creating PDF files with Dropbox Sign
+
+This is a simple example repo with a few code snippets you can use to create signable PDF files with [Dropbox Sign](https://www.dropbox.com/hellosign).
+
+# Getting up and running
+
+1. Clone the project from Github
+2. Create a virtual environment using `python3 -m venv .venv`
+3. Activate the virtual environment: `source .venv/bin/activate`
+4. Install the required modules: `pip install -r requirements.txt`
+5. Sign up for a [Dropbox plan](https://www.dropbox.com/one/try) that supports API access
+6. Copy `config.defaults` to `config.py`
+7. Fill in the values in `config.py`
+8. Run `main.py`
+
+Your Linux or Windows environment may be different from mine, so anything from Python variables, system paths, environment variables and/or the availability of `pip` might differ.
+
+YMMV.
+
+This is merely an example project for myself and others to play with, not a product that needs to install perfectly every time.
---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+This is the main Python script for signing a PDF file using the Dropbox Sign API
+"""
+
+# Imports
+import sys
+
+# pretty print makes JSON responses look good on the console output
+from pprint import pprint
+
+# Import config.py config.py is in .gitignore so that it doesn't get pushed to Git repo
+import config
+
+# import different Dropbox Sign modules
+from dropbox_sign import \
+    ApiClient, ApiException, Configuration, apis, models
+    
+# Global variables
+configuration = Configuration(
+    # Configure HTTP basic authorization: api_key
+    username=config.API_KEY,
+
+    # or, configure Bearer (JWT) authorization: oauth2
+    # access_token="YOUR_ACCESS_TOKEN",
+)
+
+# Class declarations
+
+# Function declarations
+
+def main():
+    with ApiClient(configuration) as api_client:
+        signature_request_api = apis.SignatureRequestApi(api_client)
+
+        signer_1 = models.SubSignatureRequestTemplateSigner(
+            role="Friend",
+            email_address=config.SIGNER_EMAIL_ADDRESS,
+            name="Good Friend",
+        )
+
+        signing_options = models.SubSigningOptions(
+            draw=True,
+            type=True,
+            upload=True,
+            phone=True,
+            default_type="draw",
+        )
+
+        data = models.SignatureRequestSendWithTemplateRequest(
+            template_ids=config.TEMPLATE_IDS,
+            subject="Friend Contract",
+            message="So glad you're my friend",
+            signers=[signer_1],
+            signing_options=signing_options,
+            test_mode=True,
+        )
+
+        try:
+            response = signature_request_api.signature_request_send_with_template(data)
+            pprint(response)
+        except ApiException as e:
+            print("Exception when calling Dropbox Sign API: %s\n" % e)
+
+# Main body
+if __name__ == '__main__':
+    main()
